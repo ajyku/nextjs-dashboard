332cc7ecbdfbf8d0d7a8eca44584708e6b5f42a1
Ajay Kumar <ajaykumarmail@gmail.com>
2024-06-27 21:11:31 +0530

    error handling

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,7 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = useDebouncedCallback((term: string) =>{
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
     if (term) {
@@ -18,7 +18,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 300)
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
